85b7bd0f9ba6cda4e068b90a5665ac344efadef5
Leonardo Roese <leo.roese@hotmail.com>
2022-01-08 13:36:02 -0600

    Fix tests

--- packages/config/jest-server.js
+++ packages/config/jest-server.js
@@ -2,13 +2,12 @@ module.exports = {
   ...require('./jest-common'),
   testEnvironment: 'node',
   setupFilesAfterEnv: ['@testing-library/jest-dom'],
-  collectCoverageFrom: ['**/src/**/*.{js,ts}'],
+  collectCoverageFrom: ['src/**/*.{js,ts}'],
   moduleFileExtensions: ['js', 'json', 'ts'],
   transform: {
     '^.+\\.ts$': 'esbuild-jest',
     '^.+\\.js$': 'esbuild-jest',
   },
   coveragePathIgnorePatterns: [],
-  collectCoverageFrom: ['**/src/**/*.{js,ts}'],
   coverageThreshold: null,
 }
